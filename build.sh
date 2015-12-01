473cee0a87647d7709288902480d1291ffcdcc40
Jeff Palladino <jpalladino@monetate.com>
2015-12-01 17:48:54 -0500

    initial setup

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ function error_handler() {
     echo "Error during build!"
     exit 666
 }
- 
+
 trap error_handler EXIT
 
 ./setup_build_env.sh
@@ -25,7 +25,7 @@ fi
 if [ ! -e jsoncpp/build/lib/libjsoncpp.a ]; then
     pushd jsoncpp
     # go back to an older version of cmake
-    git revert --no-edit ae5a56f9ff6932feb641bafe323195b6aa30bd21
+    # git revert --no-edit ae5a56f9ff6932feb641bafe323195b6aa30bd21
     rm -rf build
     mkdir build
     pushd build
@@ -50,7 +50,7 @@ fi
 ./qt_install/bin/qmake ichabod.pro
 make
 
-./test.sh 
+./test.sh
 
 ./archive_src.sh
 
--- setup_build_env.sh
+++ setup_build_env.sh
@@ -1,8 +1,8 @@
 
 #!/bin/bash
 
-if cat /etc/issue | grep CentOS > /dev/null; then
-    yum install -y cmake netpbm-devel python-multiprocessing gcc44-c++ libX11-devel libXext-devel libXrender-devel freetype-devel fontconfig-devel xorg-x11-xfs xorg-x11-xfs-utils rpm-build libtool.i386 automake autoconf m4
+if cat /etc/issue | grep "Amazon Linux AMI" > /dev/null; then
+    yum install -y cmake netpbm-devel python-multiprocessing gcc44-c++ libX11-devel libXext-devel libXrender-devel freetype-devel fontconfig-devel xorg-x11-xfs xorg-x11-xfs-utils rpm-build libtool.i386 automake autoconf m4 flex bison gperf libicu-devel libxslt-devel ruby
 
     if [ ! -e /usr/bin/g++ ]; then
         ln -s /usr/bin/g++44 /usr/bin/g++
