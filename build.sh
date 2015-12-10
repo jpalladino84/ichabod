3a7c141f23d213be0dacc4c9e9d79f7e60a3dfdd
Jeff Palladino <jpalladino@monetate.com>
2015-12-10 13:26:20 -0500

    update build and setup_build_env

--- build.sh
+++ build.sh
@@ -38,15 +38,24 @@ fi
 if [ ! -e qt_install ]; then
     mkdir -p qt_build
     pushd qt_build
-    # ../qt/configure -v -continue -opensource -confirm-license -release -static -qt-xcb -no-sql-ibase -no-sql-mysql -no-sql-odbc -no-sql-psql -no-sql-sqlite -no-sql-sqlite2 -xrender -largefile -rpath -openssl -D ENABLE_VIDEO=0 -nomake examples --prefix=../../qt_install || die "cannot configure qt"
     ../qt/configure -v -continue -opensource -confirm-license -release -static -no-strip -qt-xcb -qt-zlib -qt-libpng -qt-libjpeg -no-sse2 -gui -widgets -no-accessibility -no-xinput2 -no-pulseaudio -no-qml-debug -opengl -no-sql-ibase -no-sql-mysql -no-sql-odbc -no-sql-psql -no-sql-sqlite -no-sql-sqlite2 -nomake examples -nomake tests -silent -xrender -largefile -rpath -openssl -no-dbus -no-nis -no-cups -no-iconv -no-pch -no-gtkstyle -no-sm -no-xshape -no-xinerama -no-xcursor -no-xfixes -no-xrandr -no-mitshm -no-xinput -no-xkb -no-glib -no-gstreamer -D ENABLE_VIDEO=0 -no-openvg -no-xsync -no-audio-backend -no-sse3 -no-ssse3 -no-sse4.1 -no-sse4.2 -no-avx -no-avx2 -no-mips_dsp -no-mips_dspr2 -skip qtandroidextras -skip qttranslations -skip qtdoc -skip qtwinextras -skip qtmacextras -skip qtwebkit-examples -skip qtpim --prefix=../../qt_install || die "cannot configure qt"
-    if ! make -j8 -q; then
-        make -j8 || die "cannot make qt"
+    if ! gmake -j8 -q; then
+        gmake -j8 || die "cannot make qt"
     fi
-    make install || die "cannot install qt"
+    gmake install || die "cannot install qt"
     popd
     #rm -rf qt_build
 fi
+
+./qt_install/bin/qmake ~/ichabod/qt/qtwebkit/WebKit.pro
+make && make install
+
+
+./qt_install/bin/qmake ~/ichabod/qt/qtxmlpatterns/qtxmlpatterns.pro
+make && make install
+
+./qt_install/bin/qmkae ~/ichabod/qt/qtsvg/qtsvg.pro
+make && make install
 
 ./qt_install/bin/qmake ichabod.pro
 make
--- setup_build_env.sh
+++ setup_build_env.sh
@@ -2,7 +2,18 @@
 #!/bin/bash
 
 if cat /etc/issue | grep "Amazon Linux AMI" > /dev/null; then
-    yum install -y cmake netpbm-devel python-multiprocessing gcc44-c++ libX11-devel libXext-devel libXrender-devel freetype-devel fontconfig-devel xorg-x11-xfs xorg-x11-xfs-utils rpm-build libtool.i386 automake autoconf m4 flex bison gperf libicu-devel libxslt-devel ruby
+
+    # install deps
+    yum install perl-version
+
+    yum groupinstall "Developer Tools"
+
+    yum install libxcb libxcb-devel xcb-util xcb-util-devel
+    yum install flex bison gperf libicu-devel libxslt-devel ruby
+    yum install "qt5-*"
+    yum install libgcrypt-devel libgcrypt pciutils-devel nss-devel libXtst-devel gperf cups-devel pulseaudio-libs-devel libgudev1-devel systemd-devel libcap-devel alsa-lib-devel flex bison ruby gcc-c++ dbus libXrandr-devel libXcomposite-devel libXcursor-devel dbus-devel fontconfig-devel
+
+    yum install -y cmake netpbm-devel python-multiprocessing gcc44-c++ libX11-devel libXext-devel libXrender-devel freetype-devel fontconfig-devel xorg-x11-xfs xorg-x11-xfs-utils rpm-build libtool.i386 automake autoconf m4
 
     if [ ! -e /usr/bin/g++ ]; then
         ln -s /usr/bin/g++44 /usr/bin/g++
