84937fd56e3152bb5d089c9b51136c77e691a190
Jeff Palladino <jpalladino@monetate.com>
2015-12-14 14:20:13 -0500

    Polishing and adding documentation to Ichabods build script

--- build.sh
+++ build.sh
@@ -35,9 +35,16 @@ if [ ! -e jsoncpp/build/lib/libjsoncpp.a ]; then
     popd
 fi
 
-if [ ! -e qt_install ]; then
-    mkdir -p qt_build
-    pushd qt_build
+############
+# Building Qt.
+# NB: Compile and install from /mnt/ where there is
+#     generally more room on AMIs.
+######################
+
+if [ ! -e /mnt/qt_install ]; then
+    mkdir -p /mnt/qt_build
+    pushd /mnt/qt_build
+    # skiping all modules for now and just installing base Qt.
     ~/ichabod/qt/configure -v -continue -opensource -confirm-license -release -static -no-strip -qt-xcb -qt-zlib -qt-libpng -qt-libjpeg -no-accessibility -no-xinput2 -no-pulseaudio -no-qml-debug -opengl -no-sql-ibase -no-sql-mysql -no-sql-odbc -no-sql-psql -no-sql-sqlite -no-sql-sqlite2 -nomake examples -nomake tests -silent -xrender -largefile -rpath -openssl -no-dbus -no-nis -no-cups -no-iconv -no-pch -no-gtkstyle -no-sm -no-xshape -no-xinerama -no-xcursor -no-xfixes -no-xrandr -no-mitshm -no-xinput -no-xkb -no-glib -no-gstreamer -D ENABLE_VIDEO=0 -no-openvg -no-xsync -no-audio-backend -no-sse3 -no-ssse3 -no-sse4.1 -no-sse4.2 -no-avx -no-avx2 -no-mips_dsp -no-mips_dspr2 -skip qtandroidextras -skip qtmacextras -skip qtwinextras -skip qtactiveqt -skip qtlocation -skip qtimageformats -skip qtqa -skip qtx11extras -skip qtdeclarative -skip qtwayland -skip qtscript -skip qtquick1 -skip qtserialport -skip qttranslations -skip qttools -skip qtconnectivity -skip qtmultimedia -skip qtsensors -skip qtsystems -skip qtwebchannel -skip qtwebsockets -skip qtdoc -skip qtwebkit-examples -skip qtpim -skip qtxmlpatterns -skip qtsvg -skip qtwebkit --prefix=../../qt_install || die "cannot configure qt"
     if ! gmake -j8 -q; then
         gmake -j8 || die "cannot make qt"
@@ -47,18 +54,46 @@ if [ ! -e qt_install ]; then
     #rm -rf qt_build
 fi
 
-./qt_install/bin/qmake ~/ichabod/qt/qtwebkit/WebKit.pro
+###########
+# Build all required modules.
+######################
+
+# Build and install qtwebkit module
+if [ ! -e /mnt/qtwebkit_build ]; then
+    mkdir /mnt/qtwebkit_build
+    pushd /mnt/qtwebkit_build
+    /mnt/qt_install/bin/qmake ~/ichabod/qt/qtwebkit/WebKit.pro
+    make && make install
+    popd
+fi
+
+# Build and install qtsvg module
+if [ ! -e /mnt/qtsvg_build ]; then
+    mkdir /mnt/qtsvg_build
+    pushd /mnt/qtwebkit_build
+    /mnt/qt_install/bin/qmake ~/ichabod/qt/qtsvg/qtsvg.pro
+    make && make install
+    popd
+fi
+
+# Build and install qtxmlpatterns module
+if [ ! -e /mnt/qtxmlpatterns ]; then
+    mkdir /mnt/qtxmlpatterns_build
+    pushd /mnt/qtxmlpatterns_build
+    /mnt/qt_install/bin/qmake ~/ichabod/qt/qtxmlpatterns/qtxmlpatterns.pro
+    make && make install
+    popd
+fi
+
+############
+# Build and install Ichabod.
+######################
+
+/mnt/qt_install/bin/qmake ichabod.pro
 make && make install
 
-./qt_install/bin/qmake ~/ichabod/qt/qtsvg/qtsvg.pro
-make && make install
 
-./qt_install/bin/qmake ~/ichabod/qt/qtxmlpatterns/qtxmlpatterns.pro
-make && make install
-
-./qt_install/bin/qmake ichabod.pro
-make
-
+# test
 ./test.sh
 
 ./archive_src.sh
